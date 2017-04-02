7c06df4457c2d736e788b021272e5aa386c549cc
jlam17 <jlam1618@gmail.com>
2017-04-02 18:09:04 -0400

    starting to build out master API

--- master/master.go
+++ master/master.go
@@ -17,6 +17,8 @@ import (
     "config"
     "encoding/json"
 	"time" 
+	"log"
+	"sync"
     // "strings"
     // "reflect"
 )
@@ -27,8 +29,105 @@ type ServerState struct {
 	active		bool
 }
 
-var servers [10]ServerState
-var expire chan int 
+var expiredServers 	chan int 
+var servers			map[int]*ServerState
+var activeWork		bool 
+var muServers		sync.Mutex
+var muJobs			sync.Mutex
+
+const create = "create"
+const reset	 = "reset"
+const close  = "close"
+
+func finishChunk(w http.ResponseWriter, req *http.Request) {
+	// pass 
+}
+
+// look at mapreduce code 
+func submitJob(w http.ResponseWriter, req *http.Request) {
+	muJobs.Lock() 
+	avail_workers := currentServers()
+	for {
+		// hand out job to available worker 
+		select {
+		//case worker becomes available
+			// check for next chunk to send 
+		//case timer on worker expired while doing work 
+			// send work to another worker 
+		}
+	}
+	
+
+
+	muJobs.Unlock()
+    // params := req.URL.Query()
+    // for k, v := range params {
+    //     fmt.Println(k, " ", v)
+    // }
+}
+
+func join(w http.ResponseWriter, req *http.Request) {
+	conf, err := decodeRequest(req) 
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
+	uid := conf.Id.UID
+	
+	if _, ok := servers[uid]; ok {
+		log.Fatal("Intiailzing a worker that exists...")
+	}
+
+	fmt.Printf("Worker %v joining master\n", conf.Id.UID)
+
+	// Initialize worker mapping  
+	initializeWorker(uid)
+	servers[uid].timeout = setTimer(create, 1, servers[uid].timeout)
+
+	// go routine to notify master when client heartbeat expires 
+	go func(server_id int, timeout *time.Timer) {
+		for {
+			select {
+			case <- timeout.C:
+				fmt.Printf("Deleting worker from mapping\n")
+				delete(servers, server_id)
+				expiredServers <- uid
+			}
+		}
+	}(uid, servers[uid].timeout)
+
+    // Receipt for client joining the party
+    io.WriteString(w, "Welcome to the party, "+conf.Id.Alias+"\n")
+}
+
+func incomingHeartbeat(w http.ResponseWriter, req *http.Request) {
+	conf, err := decodeRequest(req)
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
+	uid := conf.Id.UID
+
+	if _, ok := servers[uid]; !ok {
+		log.Fatal("Worker expired...reinitialze worker...")
+	}
+
+	// Reset heartbeat timer for worker 
+	servers[uid].timeout = setTimer(reset, 1, servers[uid].timeout)
+
+}
+
+func currentServers() [] int {
+	muServers.Lock()
+	servers := make([]int, len(servers))
+	muServers.Unlock()
+	i := 0 
+	for key := range servers {
+		servers[i] = key 
+		i++
+	}
+	return servers 
+}
 
 // https://blog.golang.org/context/userip/userip.go
 func getIP(req *http.Request) (net.IP, error) {
@@ -41,129 +140,57 @@ func getIP(req *http.Request) (net.IP, error) {
 
     if userIP == nil {
         return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
-    } else {
-        fmt.Println("ip: ",string(ip))
     }
     return userIP, nil
 }
 
-// Not currently used
-func handler(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Master up and running!", req.URL.Path[1:])
-    // ip, _ := getIP(req)
-
-    req.Header.Get("x-forwarded-for")
-}
-
-func submitJob(w http.ResponseWriter, req *http.Request) {
-    params := req.URL.Query()
-    for k, v := range params {
-        fmt.Println(k, " ", v)
-    }
-}
-
-func join(w http.ResponseWriter, req *http.Request) {
-    // Decode configuration passed as a parameter
+func decodeRequest(req *http.Request) (config.Configuration, error) {
     var conf config.Configuration
     err := json.NewDecoder(req.Body).Decode(&conf)
-	fmt.Printf("Worker %v joining master\n", conf.Id.UID)
-
-    if err != nil {
-        http.Error(w, err.Error(), 400)
-        return
-    }
-
-    // Get client's IP address
-    _, err = getIP(req)
-
-    if err != nil {
-		fmt.Println(err)
-    }
-
-	// Initialize heartbeat timer for client 
-	uid := conf.Id.UID
-	servers[uid].timeout = setTimer(false, 1, servers[uid].timeout)
-
-	// when timeout expires, notify master of this client 
-	go func(uid int, timeout *time.Timer) {
-		for {
-			select {
-			case <- timeout.C:
-				expire <- uid
-			}
-		}
-	}(uid, servers[uid].timeout)
-
-
-    // Receipt for client joining the party
-    io.WriteString(w, "Welcome to the party, "+conf.Id.Alias+"\n")
-
-
-    // Authenticate requester
-    // If not in auth list, reject
-    // Else, ping back that they were authenticated
-    
+	return conf, err
 }
 
-func notify(w http.ResponseWriter, req *http.Request) {
-    // Decode configuration passed as a parameter
-    var conf config.Configuration
-    err := json.NewDecoder(req.Body).Decode(&conf)
-	fmt.Printf("Worker %v heartbeat\n", conf.Id.UID)
-
-    if err != nil {
-        http.Error(w, err.Error(), 400)
-        return
-    }
-
-    // Get client's IP address
-    _, err = getIP(req)
-
-    if err != nil {
-        fmt.Println(err)
-    }
-
-	// Reset heartbeat timer for client 
-	uid := conf.Id.UID
-	servers[uid].timeout = setTimer(true, 1, servers[uid].timeout)
-
-    // Receipt for client joining the party
-    // io.WriteString(w, "Notified the party, "+conf.Id.Alias+"\n")
-}
-
-func setTimer(stop bool, length int, timeout *time.Timer) *time.Timer {
-	if stop {
+func setTimer(command string, length int, timeout *time.Timer) *time.Timer {
+	switch command {
+	case close: 
+		timeout.Stop()
+	case reset:	
 		timeout.Stop()
 		timeout.Reset(time.Duration(length)* time.Second)
-	} else {
+	case create:
 		timeout = time.NewTimer(time.Duration(length)* time.Second)
 	}
-	return timeout
+	return timeout 
 }
 
 
-func heartbeat() {
+func notifyExpired() {
 	for {
 		select {
-		case server_id := <- expire:
+		case server_id := <- expiredServers:
 			fmt.Printf("Timer expired on worker %v\n", server_id)
 		} 
 	}
 }
 
-func initialize() {
-	expire = make(chan int) 
-	// resetTimer = make(chan int) 
+func initializeMaster() {
+	servers = make(map[int]*ServerState)
+	expiredServers = make(chan int) 
+	activeWork = false 
+}
+
+func initializeWorker(worker_id int) {
+	var init_state ServerState  
+	servers[worker_id] = &init_state
 }
 
 func main() {
 	fmt.Println("Listening on port 8080...")
-	initialize()
-	go heartbeat()
+	initializeMaster()
+	go notifyExpired()
 
-    // http.HandleFunc("/", handler)
     http.HandleFunc("/submitjob", submitJob)
     http.HandleFunc("/join", join)
-	http.HandleFunc("/notify", notify) 
+	http.HandleFunc("/heartbeat", incomingHeartbeat) 
     http.ListenAndServe(":8080", nil)
 }
